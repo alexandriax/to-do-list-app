4f33f745ae8fbf91a3e236fb2214ad0b017e423f
alexandriax <allyhamilton0@gmail.com>
2024-05-12 22:31:00 -0400

    add error /js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,9 @@ function newItem(){
   let li = $('<li></li>');
   let inputValue = $('#input').val();
   li.append(inputValue);
+
+  if(inputValue === '') {
+    alert("you must write something!");
+  } else {
+    $('#list').append(li);
+  }
